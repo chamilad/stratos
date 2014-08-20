15f3df83646afbd0da9d78732db924dd7d4f2865
Chamila de Alwis <chamilad@wso2.com>
2014-08-21 03:26:54 +0530

    Modified php cookbook's attribute structure, Modified agent templates and partial templates,

--- tools/chef/agent/providers/push_templates.rb
+++ tools/chef/agent/providers/push_templates.rb
@@ -7,6 +7,7 @@ action :push do
 		template "#{@current_resource.target}/#{template_name}" do
 			owner @current_resource.owner
 			group @current_resource.group
+			variables :carbon_home => @current_resource.target
 			mode '0755'
 		end
 	end
