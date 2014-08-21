929cadf9f81311e9da67105bed538364557f4f32
Chamila de Alwis <chamilad@wso2.com>
2014-08-21 22:11:23 +0530

    Added agent default attributes to tomcat

--- tools/chef/tomcat/attributes/tomcat.rb
+++ tools/chef/tomcat/attributes/tomcat.rb
@@ -2,3 +2,5 @@ include_attribute "base"
 default[:tomcat][:target] = '/mnt'
 default[:tomcat][:owner] = 'root'
 default[:tomcat][:group] = 'root'
+default[:agent][:docroot] = "/mnt/apache-tomcat-#{node[:tomcat][:version]}/webapps/"
+default[:agent][:samlalias] = "/mnt/apache-tomcat-#{node[:tomcat][:version]}/webapps/"
